606a12e64d05b1d05534e47af36de4409c37f5b0
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-06-26 10:44:07 -0700

    init commit

---
+++ src/main/kotlin/info/fekri/spring_starter1/SpringStarter1Application.kt
@@ -0,0 +1,11 @@
+package info.fekri.spring_starter1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStarter1Application
+
+fun main(args: Array<String>) {
+	runApplication<SpringStarter1Application>(*args)
+}
---
+++ src/test/kotlin/info/fekri/spring_starter1/SpringStarter1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.fekri.spring_starter1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStarter1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
