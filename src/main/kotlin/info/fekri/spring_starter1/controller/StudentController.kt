4a724bc5702ae00bcbf58943342d0074925a1407
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-06-27 01:44:31 -0700

    insert `Student` data is done

--- src/main/kotlin/info/fekri/spring_starter1/controller/StudentController.kt
+++ src/main/kotlin/info/fekri/spring_starter1/controller/StudentController.kt
@@ -1,10 +1,13 @@
 package info.fekri.spring_starter1.controller
 
+import com.google.gson.Gson
 import info.fekri.spring_starter1.model.Student
 import info.fekri.spring_starter1.repository.StudentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,6 +23,17 @@ class StudentController {
     @GetMapping("/students")
     fun getAllStudents(): ResponseEntity<MutableIterable<Student>> {
         return ResponseEntity.ok(studentRepository.findAll())
+    }
+
+    @PostMapping("/students")
+    fun insertStudent(@RequestBody data: String): ResponseEntity<String> {
+
+        val gson = Gson()
+        val newStudent = gson.fromJson<Student>(data, Student::class.java)
+
+        studentRepository.save(newStudent)
+
+        return ResponseEntity.ok("sucess")
     }
 
 }
